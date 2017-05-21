798172592dc820e25779b556ecb64c8a52d51134
Trusov Ilya <artgrosvil@gmail.com>
2017-05-21 23:16:19 +0300

    bugs fixed

--- app.py
+++ app.py
@@ -16,27 +16,28 @@ def contact():
 
 @app.route('/search', methods=['POST'])
 def search():
-    main_par = {
-        'query': request.form['query'],
-        'lan': request.form['lan'],
-        'documents': request.form['documents'],
-        'date': request.form['date'],
-        'links': request.form['links'],
-        'search_depth': request.form['search_depth']
-    }
+    if request.method == 'POST':
+        main_par = {
+            'query': request.form['query'],
+            'lan': request.form['lan'],
+            'documents': request.form['documents'],
+            'date': request.form['date'],
+            'links': request.form['links'],
+            'search_depth': request.form['search_depth']
+        }
 
-    list_url_sites = TimlParse.parse_serch_url(main_par)
-    html_data_site = TimlParse.parse_html_site(list_url_sites)
-    soup_data_site = TimlParse.research_html_site(html_data_site)
-    text_site = TimlParse.get_data_text(soup_data_site)
-    lines_text = TimlFind.break_lines(text_site)
-    stat_index = TimlFind.find_query(lines_text, main_par)
-    research_stat_index = TimlFind.research_find_query(stat_index)
-    sort_stat_index = TimlFind.sort_find_query(research_stat_index)
-    depth_stat_index = TimlFind.depth_find_query(sort_stat_index, main_par)
-    result_text = TimlFind.get_text_index(depth_stat_index, lines_text)
+        list_url_sites = TimlParse.parse_serch_url(main_par)
+        html_data_site = TimlParse.parse_html_site(list_url_sites)
+        soup_data_site = TimlParse.research_html_site(html_data_site)
+        text_site = TimlParse.get_data_text(soup_data_site)
+        lines_text = TimlFind.break_lines(text_site)
+        stat_index = TimlFind.find_query(lines_text, main_par)
+        research_stat_index = TimlFind.research_find_query(stat_index)
+        sort_stat_index = TimlFind.sort_find_query(research_stat_index)
+        depth_stat_index = TimlFind.depth_find_query(sort_stat_index, main_par)
+        result_text = TimlFind.get_text_index(depth_stat_index, lines_text)
 
-    return render_template('result.html', result_text)
+        return render_template('result.html', data=result_text)
 
 
 if __name__ == '__main__':
--- static/dev/js/custom.js
+++ static/dev/js/custom.js
@@ -6,22 +6,4 @@ $(document).ready(function () {
             $(this).parent("li").addClass("active");
         }
     });
-
-    $("#search").submit(function () {
-        $.ajax({
-            type: $("#search").attr("method"),
-            url: $("#search").attr("action"),
-            data: $("#search").serialize(),
-            success: function (data) {
-                data = JSON.parse(data);
-                for (var i = 0; i < success.length; ++i) {
-                    $('#result_search').append(success[i].text);
-                }
-            },
-            error: function (data) {
-
-            }
-        });
-        return false;
-    });
 });
